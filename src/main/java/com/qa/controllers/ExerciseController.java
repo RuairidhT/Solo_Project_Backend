cb450d8d97b024354d45c00c7fb5dfa09df131c4
RuairidhT <ruairidht2@gmail.com>
2019-09-09 15:19:29 +0100

    GET, POST and DELETE methods added

---
+++ src/main/java/com/qa/controllers/ExerciseController.java
@@ -0,0 +1,41 @@
+package com.qa.controllers;
+
+
+import com.qa.models.Exercise;
+import com.qa.repository.ExerciseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin()
+public class ExerciseController {
+
+    @Autowired
+    private ExerciseRepository repository;
+
+    @RequestMapping(value = "exercises", method = RequestMethod.GET)
+    public List<Exercise> listAllExercises(){
+        return repository.findAll();
+    }
+
+    @RequestMapping(value = "exercises", method = RequestMethod.POST)
+    public Exercise addNote(@RequestBody Exercise exercise){
+        return repository.saveAndFlush(exercise);
+    }
+
+    //delete by ID
+    @RequestMapping(value = "exercises/{id}", method = RequestMethod.DELETE)
+    public Exercise deleteNote(@PathVariable Long id){
+        Exercise existing = repository.findOne(id);
+        repository.delete(existing);
+        return existing;
+    }
+
+//    @RequestMapping(value = "exercises/name/{name}", method = RequestMethod.GET)
+//    public Exercise getNote(@PathVariable("name") String name){
+//        return repository.findOne(name);
+//    }
+
+}
---
+++ src/main/java/com/qa/models/Exercise.java
@@ -0,0 +1,58 @@
+package com.qa.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Exercise {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.AUTO)
+    Long id;
+    String name;
+    String muscleGroup;
+    String description;
+    String tutorial;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMuscleGroup() {
+        return muscleGroup;
+    }
+
+    public void setMuscleGroup(String muscleGroup) {
+        this.muscleGroup = muscleGroup;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTutorial() {
+        return tutorial;
+    }
+
+    public void setTutorial(String tutorial) {
+        this.tutorial = tutorial;
+    }
+}
